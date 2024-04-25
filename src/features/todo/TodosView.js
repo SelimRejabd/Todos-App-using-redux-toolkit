7cd2c94cdaf55a987122eb37d05b3bc90ac6bd78
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-04-25 21:25:26 +0600

    tailwind css design added

--- src/features/todo/TodosView.js
+++ src/features/todo/TodosView.js
@@ -4,22 +4,25 @@ import { fetchTodos } from './TodoSlice';
 
 const TodosView = () => {
     const {isLoading, todos, error} = useSelector((state) => state.todos);
+    console.log(isLoading);
     console.log(todos.todos);
     const dispatch = useDispatch();
     useEffect(()=>{
         dispatch(fetchTodos());
-    },);
+    },[dispatch]);
     return (
         <div>
-            <h1 className='text-red-500'>Todos</h1>
-            {isLoading & <h3>Loading...</h3>}
+            <h1 className='h-[70px] text-6xl mb-8 mt-5 rounded-lg shadow hover:bg-gray-700'>Todos</h1>
+            {isLoading && <h3>Loading...</h3>}
+    
             {error && <h3>{error}</h3>}
-            {todos && todos && todos.map((todo)=>{
-                return<article>
-                    <p>{todo.id}</p>
-                    <p>{todo.title}</p>
-                </article>
+            <div className='flex flex-wrap gap-4'>
+            {todos &&  todos.map((todo)=>{
+                return<div className='w-[300px] h-[200px] p-6 bg-white border rounded-lg dark:hover:bg-gray-700'>
+                    <p>{todo.id}. {todo.title}</p>
+                </div>
             })}
+            </div>
             
         </div>
     );
